627e779776c62859289f1fe2a4430174a2bfc4bc
kibumpark <top5w5@naver.com>
2025-02-24 12:49:53 +0900

    list 추가

--- frontend/src/components/Blog.vue
+++ frontend/src/components/Blog.vue
@@ -1,12 +1,26 @@
 <template>
     <div>
         <String label="이름" v-model="value.name" :editMode="editMode"/>
-        <String label="VisitName" v-model="value.visitName" :editMode="editMode"/>
+        <v-divider class="border-opacity-50 my-divider my-2"></v-divider>
+        <div variant="outlined" class="my-2">
+            <String label="VisitName" v-model="visitNameInput" :editMode="editMode" @keydown.enter="addVisitName"/>
+            <v-card v-if="value.visitName.length > 0" variant="outlined" class="pa-4 mt-2">
+                <v-card-sub-title>VisitName List</v-card-sub-title>
+                <li v-for="(name, index) in value.visitName" :key="index">{{ name }}</li>
+            </v-card>
+            <v-row class="ma-0 pa-0 mt-2">
+                <v-spacer></v-spacer>
+                <v-btn @click="addVisitName">VisitName 추가</v-btn>
+            </v-row>
+            <StringDetailGrid label="VisitName" offline v-model="value.visitName" :editMode="editMode" @change="change"/>
+        </div>
+        <v-divider class="border-opacity-50 my-divider my-2"></v-divider>
         <PostId offline label="postId" v-model="value.postId" :editMode="editMode" @change="change"/>
-        <StringDetailGrid label="VisitName" offline v-model="value.visitName" :editMode="editMode" @change="change"/>
+        
 
-        <v-divider class="border-opacity-100 my-divider"></v-divider>
-        <v-layout row justify-end>
+        <v-divider class="border-opacity-50 my-divider my-2"></v-divider>
+        <v-row class="ma-0 pa-0">
+            <v-spacer></v-spacer>
             <v-btn
                 width="64px"
                 color="primary"
@@ -14,7 +28,7 @@
             >
                 저장
             </v-btn>
-        </v-layout>
+        </v-row>
     </div>
 </template>
 
@@ -30,13 +44,23 @@ export default {
     },
     
     data: () => ({
-        path: "blogs"
+        path: "blogs",
+        visitNameInput: '',
+        value: {
+            visitName: [],
+        }
     }),
     created(){
     },
     computed:{
     },
     methods: {
+        addVisitNameList() {
+            if (this.visitNameInput.trim() !== '') {
+                this.value.visitName.push(this.visitNameInput.trim());
+                this.visitNameInput = '';
+            }
+        },
     },
 }
 </script>
