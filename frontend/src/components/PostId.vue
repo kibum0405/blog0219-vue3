f7b01546d1cae1908a4547b294e545c366328ef6
kibumpark <top5w5@naver.com>
2025-02-25 13:40:16 +0900

    POSTID수정

--- frontend/src/components/PostId.vue
+++ frontend/src/components/PostId.vue
@@ -1,9 +1,16 @@
 <template>
     <div>
-        <!-- namefield의 값은 참조하는 어그리거트의 속성값중 하나로 변경하면 변경한 속성에 맞는 필드명이 표시됩니다. -->
-        <BasePicker v-if="editMode" searchApiPath="posts" searchParameterName="number"  idField="id" nameField="number" path="posts" label="PostId" v-model="value" @selected="pick" :editMode="editMode" />
-        <div v-else style="height:100%">
-            <span>{{ value && value.number ? value.number : '' }}</span>
+        <!-- namefield의 값은 참조하는 어그리거트의 속성값중 하나로 변경하면 변경한 속성에 맞는 필드명이 표시됩니다.  ex)nameField="속성값" <span>{{ value && value.속성값 ? value.속성값 : '' }}</span> -->
+        <div v-if="Array.isArray(value)">
+            <div v-for="(item, index) in value" :key="index">
+                <span>{{ item.number ? item.number : '' }}</span>
+            </div>
+        </div>
+        <div v-else>
+            <BasePicker v-if="editMode" searchApiPath="posts" searchParameterName="number"  idField="id" nameField="number" path="posts" label="PostId" v-model="value" @selected="pick" :editMode="editMode" />
+            <div v-else style="height:100%">
+                <span>{{ value && value.number ? value.number : '' }}</span>
+            </div>
         </div>
     </div>
 
@@ -30,10 +37,19 @@ export default {
             this.change();
         },
     },
-    async created(){
-        this.value = this.modelValue
-        if (this.value && this.value.id !== undefined) {
-            this.value = await this.repository.findById(this.value.id)
+    async created() {
+        if (Array.isArray(this.modelValue)) {
+            this.value = await Promise.all(this.modelValue.map(async (item) => {
+                if (item && item.id !== undefined) {
+                    return await this.repository.findById(item.id);
+                }
+                return item;
+            }));
+        } else {
+            this.value = this.modelValue;
+            if (this.value && this.value.id !== undefined) {
+                this.value = await this.repository.findById(this.value.id);
+            }
         }
     },
     methods: {
